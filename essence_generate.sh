ce2c79dc00bcd3769cb88116102f4c6ab429c894
lasse <lkar18@student.aau.dk>
2023-05-23 13:32:55 +0200

    .

---
+++ essence_generate.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --output=/nfs/home/student.aau.dk/lkar18/slurm-output/essence-%j.out
+#SBATCH --error=/nfs/home/student.aau.dk/lkar18/slurm-output/essence-%j.err
+#SBATCH --partition=naples,dhabi
+#SBATCH --mem=15G
+#SBATCH --time=96:00:00
+#SBATCH -n 2
+
+TOPOLOGY="${1}"
+DEMANDS="${2}"
+ALGORITHM="${3}"
+OUTPUT_DIR="${4}"
+UPDATE_INTERVAL="${5}"
+TIME_SCALE="${6}"
+ESSENCE_DIR="${7}"
+SCALER="${8}"
+DEMAND_SCALER="${9}"
+WRITE_INTERVAL="${10}"
+RESULTS_FOLDER="${11}"
+FAILURE_SCENARIOS="${12}"
+cd $ESSENCE_DIR
+
+source venv/bin/activate
+
+python3 -m pip install -r requirements.txt
+python3 main.py --utilization_recording_interval 10000 --topology ${TOPOLOGY} --demands ${DEMANDS} --algorithm ${ALGORITHM} --output_dir ${OUTPUT_DIR} --update_interval ${UPDATE_INTERVAL} --time_scale ${TIME_SCALE} --scaler ${SCALER} --demand_scaler ${DEMAND_SCALER} --write_interval ${WRITE_INTERVAL} --results_folder ${RESULTS_FOLDER} --failure_scenarios ${FAILURE_SCENARIOS} --no_execution
--- schedule_essence.sh
+++ schedule_essence.sh
@@ -7,52 +7,24 @@
 # Go to INET_DIR and run "make makefiles" and "make"
 
 SLURM_OUTPUT="/nfs/home/student.aau.dk/lkar18/slurm-output/essence"
-
-#CREATE_CONFS="true"
-
-#CREATE_OMNET_INPUT="true"
-
 RUN_SIMULATION="true"
-
-#RUN_MPLS_KIT="false"
-
-PARSE_RESULTS="true"
-
 MAKE_PLOTS="true"
-
 ESSENCE_DIR="/nfs/home/student.aau.dk/lkar18/p10/"
-
-#MPLS_KIT_RESULTS_DIR="results/"
-
-TOPO_DIR="DT/topology/"
-
-CONFS_DIR="confs/"
-
-OMNET_RESULTS_DIR="/nfs/home/student.aau.dk/lkar18/deutsche/output_files/"
-
-DEMANDS_DIR="DT/demands/"
-
+TOPO_DIR="experiments/frr/"
+OMNET_RESULTS_DIR="/nfs/home/student.aau.dk/lkar18/omnet_results/"
+DEMANDS_DIR="temporal_demands/"
 PACKET_SIZE="64"
-
-SCALER="1"
-
+SCALER="10"
 ZERO_LATENCY=""
-
-UPDATE_INTERVAL="180"
-
-TIME_SCALE="1"
-
+UPDATE_INTERVAL="20"
+TIME_SCALE="0.001"
 OMNET_INPUT_FILES_DIR="/nfs/home/student.aau.dk/lkar18/omnet_input/"
-
 INET_DIR="/nfs/home/student.aau.dk/lkar18/inet/"
-
 SCRIPTS_DIR="/nfs/home/student.aau.dk/lkar18/Omnet-scripts/"
-
-PLOT_DIR="/nfs/home/student.aau.dk/lkar18/deutsche/plots/"
-
-DEMAND_SCALER="1"
-
-WRITE_INTERVAL="60"
+PLOT_DIR="/nfs/home/student.aau.dk/lkar18/plots/"
+DEMAND_SCALER="0.2"
+WRITE_INTERVAL="10"
+FAILURE_SCENARIOS=5
 
 cd $ESSENCE_DIR$TOPO_DIR
 
@@ -70,21 +42,24 @@ for i in "${!TOPOS[@]}"; do
 done
 
 # RUN SIMULATIONS
-#declare -a ALGS=("essence" "essence_precomputed" "essence_stateless" "shortest_path")
-declare -a ALGS=("shortest_path")
-PARSE_RESULTS_JOBS=":1"
+declare -a ALGS=("essence" "essence_precomputed" "essence_stateless" "shortest_path" "fbr")
+#declare -a ALGS=("shortest_path")
+RUN_OMNET_JOBS=":1"
 if [ "$RUN_SIMULATION" = "true" ]; then
     for TOPO in "${TOPOS[@]}"; do
         topo_without_type="${TOPO::-5}"
         topo_without_type_and_zoo="${topo_without_type:4}"
         for ALG in "${ALGS[@]}"; do
-            RUN_ID=$(sbatch --parsable essence.sh $TOPO_DIR$TOPO $DEMANDS_DIR${topo_without_type_and_zoo}_0000.yml $ALG $OMNET_INPUT_FILES_DIR $UPDATE_INTERVAL $TIME_SCALE $ESSENCE_DIR $SCALER $DEMAND_SCALER $WRITE_INTERVAL)
-            PARSE_ID=$(sbatch --parsable --dependency=afterok:$RUN_ID parse.sh $topo_without_type_and_zoo $ALG $OMNET_INPUT_FILES_DIR$topo_without_type_and_zoo/$ALG/results/ $OMNET_RESULTS_DIR)
-            PARSE_RESULTS_JOBS="$PARSE_RESULTS_JOBS:$PARSE_ID"
+            # First create all the necessary files
+            FILE_GENERATE_ID=$(sbatch --parsable essence_generate.sh $TOPO_DIR$TOPO $DEMANDS_DIR${topo_without_type_and_zoo}_0000.yml $ALG $OMNET_INPUT_FILES_DIR $UPDATE_INTERVAL $TIME_SCALE $ESSENCE_DIR $SCALER $DEMAND_SCALER $WRITE_INTERVAL $OMNET_RESULTS_DIR $FAILURE_SCENARIOS)
+            for ((i=0;i<FAILURE_SCENARIOS;i++)); do
+                RUN_ID=$(sbatch --parsable essence_execute.sh $TOPO_DIR$TOPO $DEMANDS_DIR${topo_without_type_and_zoo}_0000.yml $ALG $OMNET_INPUT_FILES_DIR $UPDATE_INTERVAL $TIME_SCALE $ESSENCE_DIR $SCALER $DEMAND_SCALER $WRITE_INTERVAL)
+                RUN_OMNET_JOBS="$RUN_OMNET_JOBS:$RUN_ID"
+            done
         done
     done
 fi
 
 if [ "$MAKE_PLOTS" = "true" ]; then
-    sbatch --dependency=afterok$PARSE_RESULTS_JOBS make_plots.sh $OMNET_RESULTS_DIR $PLOT_DIR
+    sbatch --dependency=afterok$RUN_OMNET_JOBS make_plots.sh $OMNET_RESULTS_DIR $PLOT_DIR
 fi
